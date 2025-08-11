234986054b42916e7d0b0e962359cddbaa381b19
vikramkkrraj <vikramkrraj9876@gmail.com>
2025-08-11 19:14:39 +0530

    cors bug

--- server/index.js
+++ server/index.js
@@ -13,7 +13,10 @@ import connectDB from './db/config.js';
 const app = express();
 const PORT = process.env.PORT || 4000
 
-app.use(cors());
+app.use(cors({
+  origin: "https://portfolio-rgq3-i0zjxn5ud-vikram-s-projects-bac60270.vercel.app/", // your frontend URL
+  credentials: true,               // allow cookies to be sent
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'))
