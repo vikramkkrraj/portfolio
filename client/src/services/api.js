d410bf82a40ddd3a6419557c48835bc1f707082a
vikramkkrraj <vikramkrraj9876@gmail.com>
2025-08-11 18:41:48 +0530

    env fix

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,11 @@
 // src/services/api.js
 import axios from "axios";
 
+console.log(import.meta.env.VITE_API_BASE_URL);
+
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL ,
   withCredentials: true, // send cookies
 });
 
